f2ce7e05444c4755a943039270b743fd49d3cad8
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2025-04-01 22:26:24 +1100

    init

---
+++ build.zig
@@ -0,0 +1,37 @@
+const std = @import("std");
+const cimgui = @import("cimgui_zig");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const cimgui_dep = b.dependency("cimgui_zig", .{
+        .target = target,
+        .optimize = optimize,
+        .platform = cimgui.Platform.GLFW,
+        .renderer = cimgui.Renderer.OpenGL3,
+    });
+
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "dts_viewer",
+        .root_module = exe_mod,
+    });
+    exe.linkLibrary(cimgui_dep.artifact("cimgui"));
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
