ad87714557c9cfa80e588758011820fb35f42a3a
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2025-05-08 13:30:57 +1000

    fix?

--- build.zig
+++ build.zig
@@ -257,6 +257,7 @@ pub fn build(b: *std.Build) !void {
     make_deb.addDirectoryArg(wf.getDirectory());
     const deb_name = b.fmt("{s}.deb", .{ package.dir });
     const deb = make_deb.addOutputDirectoryArg(deb_name);
+    make_deb.dependOn(package_step);
 
     package_step.dependOn(&b.addInstallFileWithDir(deb, .{ .custom = "package" }, deb_name).step);
 }
